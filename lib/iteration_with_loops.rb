d08377d5f13056432dc9110d78d69894fd9f7f6a
Kevin Huang <khuang159@gmail.com>
2020-09-21 16:55:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,12 +2,16 @@ def join_nested_strings(src)
 str =""
 i = 0
 strArr = [] 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> c0f7028ecb10994b05b603bc1a95ea6ef1a77f2b
 while i < src.count do
     j = 0
    
     while j < src[i].count do
+<<<<<<< HEAD
       if src[i][j].class == str.class
         strArr << src[i][j]
       end
@@ -18,6 +22,16 @@ while i < src.count do
  str = strArr.join(" ")
  return str
   
+=======
+         if (src[i][j] == str.class)
+            strArr << src[i][j]
+          end
+      j+=1
+      end
+    i+=1
+  end  
+ puts strArr
+>>>>>>> c0f7028ecb10994b05b603bc1a95ea6ef1a77f2b
 end
 
 
